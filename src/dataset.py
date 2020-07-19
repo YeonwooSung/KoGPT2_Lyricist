84f18f45e17c4ed0adeb6301ed752d9a12148c1e
YeonwooSung <neos960518@gmail.com>
2020-07-19 21:27:30 +0900

    Implement custom dataset class

---
+++ src/dataset.py
@@ -0,0 +1,69 @@
+from torch.utils.data import Dataset
+from gluonnlp.data import SentencepieceTokenizer
+import gluonnlp
+import numpy as np
+
+from prepare_pytorch_kogpt2 import load_kogpt2_model
+from utils import download, tokenizer, get_tokenizer
+
+
+
+class KoLyricsDataset(Dataset):
+	"""web novel dataset"""
+
+	def __init__(self, file_path, vocab, tokenizer):
+		self.file_path = file_path
+		self.data =[]
+		self.vocab =vocab
+		self.tokenizer = tokenizer
+		file = open(self.file_path, 'r', encoding='utf-8')
+
+		lines = file.read()
+		lines = lines.split("\n")
+
+		datasets = []
+		now = ""
+		for i, line in enumerate(lines):
+			if i % 30 == 0 and i != 0:
+				datasets.append(now)
+				now = ""
+			now = now + "\n" + line
+
+		# lines = lines.split("<|endoftext|>")
+		# lines = [line.split("\n") for line in lines]
+		# lines = [str(line) for line in lines]
+
+		print("tokenizer ending")
+		for line in datasets:
+			if not line:
+				break
+			if len(line) < 3:
+				continue
+
+			toeknized_line = tokenizer(line[:-1])
+
+			index_of_words = [vocab[vocab.bos_token], ] + vocab[toeknized_line] + [vocab[vocab.eos_token]]
+			self.data.append(index_of_words)
+
+		print(np.shape(self.data))
+
+		file.close()
+
+	def __len__(self):
+		return len(self.data)
+
+	def __getitem__(self, index):
+		item = self.data[index]
+		return item
+
+
+
+if __name__ == "__main__":
+    tok_path = get_tokenizer()
+    tok = SentencepieceTokenizer(tok_path,  num_best=0, alpha=0)
+    
+    # load model and vocabularies
+    model, vocab = load_kogpt2_model()
+
+    # create KoLyricsDataset instance
+    dataset = KoLyricsDataset('../dataset/lyrics_dataset.txt', vocab, tok)
