8589066dcbbe59d8423a471290e3993388db26f4
YeonwooSung <neos960518@gmail.com>
2020-08-02 16:37:32 +0900

    Add generate.py to generate result texts

---
+++ src/generate.py
@@ -0,0 +1,101 @@
+import os
+import torch
+import argparse
+
+import gluonnlp
+from gluonnlp.data import SentencepieceTokenizer
+
+
+from dataset import KoLyricsDataset
+from utils import get_tokenizer, download, tokenizer
+from prepare_pytorch_kogpt2 import get_pytorch_kogpt2_model, load_kogpt2_model, load_kogpt2_model_from_checkpoint
+from sample import sample_sequence
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--temperature', type=float, default=0.7,
+					help="temperature 를 통해서 글의 창의성을 조절합니다.")
+parser.add_argument('--top_p', type=float, default=0.9,
+					help="top_p 를 통해서 글의 표현 범위를 조절합니다.")
+parser.add_argument('--top_k', type=int, default=40,
+					help="top_k 를 통해서 글의 표현 범위를 조절합니다.")
+parser.add_argument('--text_size', type=int, default=250,
+					help="결과물의 길이를 조정합니다.")
+parser.add_argument('--loops', type=int, default=-1,
+					help="글을 몇 번 반복할지 지정합니다. -1은 무한반복입니다.")
+parser.add_argument('--tmp_sent', type=str, default="사랑",
+					help="글의 시작 문장입니다.")
+parser.add_argument('--load_path', type=str, default="./checkpoint/Alls/KoGPT2_checkpoint_296000.tar", help="학습된 결과물을 저장하는 경로입니다.")
+
+args = parser.parse_args()
+
+
+
+def main(temperature = 0.7, top_p = 0.8, top_k = 40, tmp_sent = "", text_size = 100, loops = 0, load_path = ""):
+	ctx = 'cuda'
+	cachedir = '~/kogpt2/'
+	save_path = './checkpoint/'
+    # load pretrained models
+    kogpt2model, _ = load_kogpt2_model(ctx=ctx)
+
+	# set device
+	device = torch.device(ctx)
+
+	kogpt2model.eval()
+	vocab_b_obj = gluonnlp.vocab.BERTVocab.from_sentencepiece(vocab_path,
+															  mask_token=None,
+															  sep_token=None,
+															  cls_token=None,
+															  unknown_token='<unk>',
+															  padding_token='<pad>',
+															  bos_token='<s>',
+															  eos_token='</s>')
+
+	tok_path = get_tokenizer()
+	model, vocab = kogpt2model, vocab_b_obj
+	tok = SentencepieceTokenizer(tok_path)
+
+	if loops:
+		num = 1
+	else:
+		num = 0
+
+	while 1:
+		sent =''
+		if tmp_sent == "":
+			tmp_sent = input('input : ')
+		sent = sent+tmp_sent
+
+		toked = tok(sent)
+
+		if len(toked) > 1022:
+			break
+
+		sent = sample_sequence(model, tok, vocab, sent, text_size, temperature, top_p, top_k)
+		sent = sent.replace("<unused0>", "\n")
+		
+		print(sent)
+
+		now = [int(n) for n in os.listdir("./samples")]
+		now = max(now)
+		f = open("samples/" + str(now + 1), 'w', encoding="utf-8")
+		head = [load_path, tmp_sent, text_size, temperature, top_p, top_k]
+		head = [str(h) for h in head]
+		f.write(",".join(head))
+		f.write("\n")
+		f.write(sent)
+		f.close()
+
+		tmp_sent = ""
+
+		if num != 0:
+			num += 1
+			if num >= loops:
+				print("good")
+				return
+
+
+if __name__ == "__main__":
+	# execute only if run as a script
+	main(temperature=args.temperature, top_p=args.top_p, top_k=args.top_k, tmp_sent=args.tmp_sent, text_size=args.text_size, loops=args.loops+1, load_path=args.load_path)
